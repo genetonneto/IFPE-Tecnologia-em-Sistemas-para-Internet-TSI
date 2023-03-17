2c652784e8b5478f0ca6e4ff493ab7947a5b7ffe
Geneton Neto <geneton.alves.neto@gmail.com>
2023-03-17 15:05:15 -0300

    mais uma vez reorganizando a numeraçao das questoes

--- Primeiro Periodo/Programação Imperativa-Logica de Programação/01 - Operadores, tipos e variaveis/Nv 01/4 - Imprimir o perímetro de um pentágono (soma de seus cinco lados).js
+++
@@ -1,9 +0,0 @@
-//  Imprimir o perímetro de um pentágono (soma de seus cinco lados);
-
-var ladoA = 10;
-var ladoB = 20;
-var ladoC = 30;
-var ladoD = 40;
-var ladoE = 50;
-var perimetro = ladoA + ladoB + ladoC + ladoD + ladoE;
-console.log(perimetro);

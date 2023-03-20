9a052f6c9f2312378eb301ae290160506e2654a9
Geneton Neto <geneton.alves.neto@gmail.com>
2023-03-20 08:14:08 -0300

    adicionando questao 8 e 9 de operadores logicos de nivel 02

---
+++ Primeiro Periodo/Programação Imperativa-Logica de Programação/03 - Operadores logicos/Nv 02/Q9 - Identificar quando exatamente uma entre duas variáveis booleanas é falsa.js
@@ -0,0 +1,36 @@
+// Identificar quando exatamente uma entre duas variáveis booleanas é falsa;
+
+/*
+Operador E ==> &&
+false && false = false
+false && true = false
+true && false = false
+true && true = true
+
+
+Operador OU ==> ||
+false || false = false
+false || true = true
+true || false = true
+true || true = true
+
+
+b1 = true   &&  b2 = !true  (entao b1 é verdade e b2 é falso)
+b1 = false  &&  b2 = !false (entao b1 é falso e b2 é verdadeiro)
+b1 = !true  &&  b2 = !true  (entao b1 é falso e b2 falso)
+b1 = !false &&  b2 = !false (entao b1 é verdadeiro e b2 é verdadeiro)
+
+*/
+
+let b1 = true;
+let b2 = true;
+
+if (b1 == true && b2 != true) {
+    console.log("b1 e igual a : " ,b1, " e b2 e :" ,b2);
+} else if (b1 == false && b2 != false) {
+    console.log("b1 e igual a : " ,b1, " e b2 e :" ,b2);
+} else if (b1 != true && b2 != true) {
+    console.log("b1 e igual a : " ,b1, " e b2 e :" ,b2);
+} else if (b1 != false && b2 != false){
+    console.log("b1 e igual a : " ,b1, " e b2 e :" ,b2);
+}

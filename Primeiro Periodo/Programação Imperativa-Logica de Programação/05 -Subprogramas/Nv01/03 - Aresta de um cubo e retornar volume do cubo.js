b8685cc913fc6c43fa31915e1716bc0619400903
Geneton Neto <geneton.alves.neto@gmail.com>
2023-05-18 21:00:30 -0300

    apagando atividade duplicada

--- Primeiro Periodo/Programação Imperativa-Logica de Programação/05 -Subprogramas/Nv01/03 - Aresta de um cubo e retornar volume do cubo.js
+++
@@ -1,8 +0,0 @@
-// Receber como parâmetro a aresta de um cubo e retornar volume do cubo;
-
-function aresta (volume) {
-    return volume ** 3;
-}
-
-resultado = aresta(3);
-console.log(resultado);
--- Primeiro Periodo/Programação Imperativa-Logica de Programação/05 -Subprogramas/Nv01/08 - Lados de um retângulo, retornar o valor da sua a área..js
+++
@@ -1,6 +0,0 @@
-// Dados os lados de um retângulo, retornar o valor da sua a área.
-
-function area(b,h) {
-    return (b * h);
-}
-console.log(area(2,3));

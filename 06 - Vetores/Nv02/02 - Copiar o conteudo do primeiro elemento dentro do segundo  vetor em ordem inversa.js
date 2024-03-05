88b472a735aae22d314ca98aec06b511c99e4f07
Geneton Neto <geneton.alves.neto@gmail.com>
2024-03-05 09:18:35 -0300

    nova resolucao

--- 06 - Vetores/Nv02/02 - Copiar o conteudo do primeiro elemento dentro do segundo  vetor em ordem inversa.js
+++ 06 - Vetores/Nv02/02 - Copiar o conteudo do primeiro elemento dentro do segundo  vetor em ordem inversa.js
@@ -1,13 +1,29 @@
 // Implementar um subprograma que dados dois vetores, copia o conteúdo do primeiro no segundo vetor (elemento por elemento) na ordem inversa;
 
-function elementos(v1, v2) {
+// function elementos(v1, v2) {
+//     for (i = 0; i < v1.length; i++) {
+//         v2[i] = v1[i];
+//     }
+//     return v2.reverse();
+
+// }
+
+// let v1 = [1, 2, 3, 4];
+// let v2 = [5, 4, 3, 'Neto'];
+// console.log(elementos(v1,v2));
+
+function parametro(v1, v2) {
+
     for (i = 0; i < v1.length; i++) {
         v2[i] = v1[i];
     }
-    return v2.reverse();
+
+    for (i = v2.length - 1; i >= 0; i--) {
+        console.log(v2[i]);
+    }
 
 }
 
-let v1 = [1, 2, 3, 4];
-let v2 = [5, 4, 3, 'Neto'];
-console.log(elementos(v1,v2));
+let v1 = [1, 3, 5, -2];
+let v2 = [1, 2, 3, 4];
+let resultado = parametro(v1, v2);
